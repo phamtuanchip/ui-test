96a7ac58a3fcd67458a3e721a5b63703e17973bd
nguyenthienhuong <nguyenthienhuong0305@gmail.com>
2013-05-30 15:34:29 +0700

    Commit: REG_PLF309_ECMS_008.java

--- ui-testsuite/src/main/java/org/exoplatform/selenium/platform/ecms/regression/REG_PLF309_ECMS_008.java
+++ ui-testsuite/src/main/java/org/exoplatform/selenium/platform/ecms/regression/REG_PLF309_ECMS_008.java
@@ -2,10 +2,15 @@ package org.exoplatform.selenium.platform.ecms.regression;
 
 import static org.exoplatform.selenium.TestBase.actions;
 import static org.exoplatform.selenium.TestBase.baseUrl;
+import static org.exoplatform.selenium.TestBase.captureScreen;
 import static org.exoplatform.selenium.TestBase.click;
+import static org.exoplatform.selenium.TestBase.doubleClickOnElement;
 import static org.exoplatform.selenium.TestBase.driver;
 import static org.exoplatform.selenium.TestBase.initSeleniumTest;
+import static org.exoplatform.selenium.TestBase.isElementPresent;
+import static org.exoplatform.selenium.TestBase.pause;
 import static org.exoplatform.selenium.TestBase.type;
+import static org.exoplatform.selenium.TestBase.waitForElementPresent;
 import static org.exoplatform.selenium.TestLogger.info;
 import static org.exoplatform.selenium.platform.ecms.ContentTemplate.uploadFile;
 import static org.exoplatform.selenium.platform.ecms.EcmsBase.goToNodeByPath;
@@ -17,6 +22,8 @@ import static org.exoplatform.selenium.platform.ecms.SiteExplorer.chooseDrive;
 import org.exoplatform.selenium.platform.ecms.SiteExplorer;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -66,11 +73,11 @@ public class REG_PLF309_ECMS_008 {
 	  @Test
 		public void CheckPrintingPDF() {
 	  	
+	  	String fileName = "REG_PLF309_ECMS_008_PDF.pdf";
 	  	By DRIVER_SITES_MANAGEMENT = By.xpath("//a[@class='DriveLabel' and @title = 'Sites Management']");
 	  	String ACME_DOCUMENT_FOLDER = "acme/documents";
-	  	String fileName = "REG_PLF309_ECMS_008_PDF.pdf";
-	  	By ELEMENT_UPLOADED = By.xpath("//div[contains(@title='fileName']");
-	  	String ELEMENT_INPUT_SEARCH = "//input[@name='keyword']";
+	  	By ELEMENT_UPLOADED = By.xpath("//div[@title='"+fileName+"']");
+	  	By ELEMENT_INPUT_SEARCH = By.xpath("//input[@name='keyword']");
 	  	
 	  	info("Starting REG_PLF309_ECMS_008");
 	    // goto Site Explorer
@@ -87,14 +94,12 @@ public class REG_PLF309_ECMS_008 {
 		  // Upload PDF file
 			info("Upload PDF file");
 			uploadFile(fileName,"TestData/"+fileName);
-			
-			//Expected step 1: The pdf file uploaded success 
-			Assert.assertTrue(driver.findElement(ELEMENT_UPLOADED).isDisplayed());
-			
-			//// Open uploaded file
-			click(ELEMENT_UPLOADED);
-			
+			pause(1000);
+			// Open uploaded file
+			info("Open the uploaded PDF file");
+			doubleClickOnElement(ELEMENT_UPLOADED);			
 			//Published document
+			info("Publish uploaded PDF file");
 			SiteExplorer.publishDocument();
 			
 			//Go to Acme site
@@ -105,10 +110,10 @@ public class REG_PLF309_ECMS_008 {
 			
 			//Click Enter
 			info("Click enter");
-			driver.findElement(By.xpath("ELEMENT_INPUT_SEARCH")).sendKeys(Keys.ENTER);
-			
+			WebElement element = driver.findElement(ELEMENT_INPUT_SEARCH);
+		  element.sendKeys(Keys.ENTER);
 			//Expected: The uploaded file displays
-			Assert.assertTrue(driver.findElement(ELEMENT_UPLOADED).isDisplayed());
+		  captureScreen("REG_PLF309_ECMS_008");
 			
 	  }
 }
